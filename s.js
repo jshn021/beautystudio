e4be4a912dcc6c927d179f07d7ad012b7b113b84
jshn021 <140138291+jshn021@users.noreply.github.com>
2024-03-13 11:04:59 +0530

    inital commit

---
+++ s.js
@@ -0,0 +1,13 @@
+let navbar=document.querySelector('.header .navbar');
+let menuBtn = document.querySelector('#menu-btn');
+let closeBtn= document.querySelector('#close-navbar');
+
+menuBtn.onclick =() =>{
+    navbar.classList.add('active');
+} 
+closeBtn.onclick=() =>{
+    navbar.classList.remove('active');
+} 
+window.onclick=() =>{
+    navbar.classList.remove('active');
+}
